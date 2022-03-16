4f6b79032ddb28caf71f2814466617d583a752af
KennetAguirreReyes <85863289+KennetAguirreReyes@users.noreply.github.com>
2022-03-15 20:57:53 -0700

    subiendo el programa

---
+++ BlockNotas/BlockNotas/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace BlockNotas
+{
+    partial class FrmBlock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbContenido = new System.Windows.Forms.RichTextBox();
+            this.openFileDialogB = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialogB = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtbContenido
+            // 
+            this.rtbContenido.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbContenido.Location = new System.Drawing.Point(185, 27);
+            this.rtbContenido.Name = "rtbContenido";
+            this.rtbContenido.Size = new System.Drawing.Size(723, 423);
+            this.rtbContenido.TabIndex = 1;
+            this.rtbContenido.Text = "";
+            // 
+            // openFileDialogB
+            // 
+            this.openFileDialogB.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(908, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(143, 26);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(143, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(143, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(143, 26);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(143, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click_1);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView1.Location = new System.Drawing.Point(0, 27);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(185, 423);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // FrmBlock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 450);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.rtbContenido);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmBlock";
+            this.Text = "Bloc de Notas";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox rtbContenido;
+        private System.Windows.Forms.OpenFileDialog openFileDialogB;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogB;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ BlockNotas/BlockNotas/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //para usar las librerias que permiten trabajar con archivos
+
+namespace BlockNotas
+{
+    public partial class FrmBlock : Form
+    {
+        public FrmBlock()
+        {
+            InitializeComponent();
+        }
+
+        public static class Global
+        {
+            public static string rutaBase;
+        }
+
+        private TreeNode CreateTree(DirectoryInfo directoryInfo)
+        {
+            TreeNode treeNode = new TreeNode(directoryInfo.Name);
+
+            foreach (var item in directoryInfo.GetFiles())
+            {
+                treeNode.Nodes.Add(new TreeNode(item.Name));
+            }
+
+            foreach (var item in directoryInfo.GetDirectories())
+            {
+                treeNode.Nodes.Add(CreateTree(item));
+            }
+
+            return treeNode;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog fbdialog = new FolderBrowserDialog();
+
+                if (fbdialog.ShowDialog() == DialogResult.OK)
+                {
+                    String globalPath = fbdialog.SelectedPath;
+                    treeView1.Nodes.Clear();
+
+                    DirectoryInfo directoryInfo = new DirectoryInfo(globalPath);
+                    Global.rutaBase = directoryInfo.FullName;
+                    treeView1.Nodes.Add(CreateTree(directoryInfo));
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al abrir");
+            }
+            
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(saveFileDialogB.FileName))
+                {
+                    string text = saveFileDialogB.FileName;
+
+                    StreamWriter SaveText = File.CreateText(text);
+                    SaveText.Write(rtbContenido.Text);
+                    SaveText.Flush(); //para liberar memoria del writer
+                    SaveText.Close(); //para cerrar recursos
+                    MessageBox.Show("Su archivo fue guardado con exito.");
+
+                }
+                else
+                {
+                    saveFileDialogB.Filter = "Text files (*.txt) | *.txt";
+                    if (saveFileDialogB.ShowDialog() == DialogResult.OK)
+                    {
+                        string text = saveFileDialogB.FileName;
+
+                        StreamWriter SaveText = File.CreateText(text);
+                        SaveText.Write(rtbContenido.Text);
+                        SaveText.Flush();
+                        SaveText.Close();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al guardar");
+            }
+
+        } 
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rtbContenido.Clear();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                saveFileDialogB.Filter = "Text files (*.txt) | *.txt";
+                if (saveFileDialogB.ShowDialog() == DialogResult.OK) //me enseña la ventana y me manda si el usuario le dio a ok o cancel
+                {
+
+                    if (File.Exists(saveFileDialogB.FileName))
+                    {
+                        string text = saveFileDialogB.FileName;
+
+                        StreamWriter SaveText = new StreamWriter(text);
+                        //StreamWriter SaveText = File.CreateText(text);
+                        //StreamWriter SaveText = System.IO.File.AppendText(text);
+                        SaveText.Write(rtbContenido.Text);
+                        SaveText.Flush(); //para liberar memoria del writer
+                        SaveText.Close(); //para cerrar recursos
+
+                    }
+                    else //sea que exista o no vamos a escribir la informacion, para que se escriba la infor y quede guardada
+                    {
+                        string text = saveFileDialogB.FileName;
+
+                        StreamWriter SaveText = File.CreateText(text);
+                        SaveText.Write(rtbContenido.Text);
+                        SaveText.Flush();
+                        SaveText.Close();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al guardar");
+            }
+        }
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string text = Global.rutaBase + "\\" + treeView1.SelectedNode.Text;
+
+            if (File.Exists(text))
+            {
+                TextReader read = new StreamReader(text);
+                rtbContenido.Text = read.ReadToEnd();
+                read.Close();
+            }
+        }
+
+    }
+}
